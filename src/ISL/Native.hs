87a0460ea3aaa21801edb73d6380d05d4ea1d547
Joel Burget <joelburget@gmail.com>
2018-12-03 20:08:16 -0800

    Add `Take` newtype and `Ptr` type aliases.
    `Once` enforces as much safety as we can get without linear types:
    
    * the `Once` data constructor is not exported so you can only obtain a
      `Once` via `copy`.
    * all raw operations require a `Once`
    
    Of course, this doesn't prevent you from passing the same `Once` to two
    consumers (that's what we need linear types for), but hopefully the
    naming is deterrent enough.

--- src/ISL/Native.hs
+++ src/ISL/Native.hs
@@ -4,7 +4,11 @@
 
 -- | An inline-c based low-level interface to isl.
 module ISL.Native
-  ( IslCopy(copy)
+  ( Once
+  , Take
+  , Give
+  , Keep
+  , IslCopy(copy)
   , IslFree(free)
 
   , ctxFree
@@ -88,11 +92,27 @@ C.include "<isl/map.h>"
 C.include "<isl/set.h>"
 C.include "<isl/space.h>"
 
+-- | A pointer that is only allowed to be used once
+newtype Once a = Once (Ptr a)
+
+-- | A pointer that isl is free to destroy
+type Take a = Once a
+
+-- | A pointer given as the result of a computation
+type Give a = Ptr a
+
+-- | A pointer used as input to a computation and not destroyed
+type Keep a = Ptr a
+
+copy' :: IslCopy a => Keep a -> Once a
+copy' = Once . copy
+
+
 class IslCopy a where
-  copy :: Ptr a -> Ptr a
+  copy :: Keep a -> Give a
 
 class IslFree a where
-  free :: Ptr a -> IO ()
+  free :: Take a -> IO ()
 
 -- __isl_take: can no longer be used
 -- __isl_keep: only used temporarily
@@ -101,20 +121,20 @@ class IslFree a where
 
 instance IslFree Ctx where free = ctxFree
 
-ctxFree :: Ptr Ctx -> IO ()
-ctxFree ctx = [C.block| void { isl_ctx_free($(isl_ctx* ctx)); } |]
+ctxFree :: Take Ctx -> IO ()
+ctxFree (Once ctx) = [C.block| void { isl_ctx_free($(isl_ctx* ctx)); } |]
 
 -- * BasicSet
 
 instance IslCopy BasicSet where copy = basicSetCopy
 instance IslFree BasicSet where free = basicSetFree
 
-basicSetCopy :: Ptr BasicSet -> Ptr BasicSet
+basicSetCopy :: Keep BasicSet -> Give BasicSet
 basicSetCopy bset =
   [C.pure| isl_basic_set* { isl_basic_set_copy($(isl_basic_set* bset)) } |]
 
-basicSetFree :: Ptr BasicSet -> IO ()
-basicSetFree bset = void
+basicSetFree :: Take BasicSet -> IO ()
+basicSetFree (Once bset) = void
   [C.block| isl_basic_set* { isl_basic_set_free($(isl_basic_set* bset)); } |]
 
 -- * Set
@@ -122,78 +142,79 @@ basicSetFree bset = void
 instance IslCopy Set where copy = setCopy
 instance IslFree Set where free = setFree
 
-setCopy :: Ptr Set -> Ptr Set
+setCopy :: Keep Set -> Give Set
 setCopy set = [C.pure| isl_set* { isl_set_copy($(isl_set* set)) } |]
 
-setFree :: Ptr Set -> IO ()
-setFree set = void [C.block| isl_set* { isl_set_free($(isl_set* set)); } |]
+setFree :: Take Set -> IO ()
+setFree (Once set) = void [C.block| isl_set* { isl_set_free($(isl_set* set)); } |]
 
-unsafeSetUnion :: Ptr Set -> Ptr Set -> Ptr Set
-unsafeSetUnion set1 set2 = [C.pure| isl_set* {
+unsafeSetUnion :: Take Set -> Take Set -> Give Set
+unsafeSetUnion (Once set1) (Once set2) = [C.pure| isl_set* {
   isl_set_union($(isl_set* set1), $(isl_set* set2))
   } |]
 
-setUnion :: Ptr Set -> Ptr Set -> Ptr Set
-setUnion set1 set2 = unsafeSetUnion (setCopy set1) (setCopy set2)
+setUnion :: Keep Set -> Keep Set -> Give Set
+setUnion set1 set2 = unsafeSetUnion (copy' set1) (copy' set2)
 
-unsafeSetSubtract :: Ptr Set -> Ptr Set -> Ptr Set
-unsafeSetSubtract set1 set2 = [C.pure| isl_set* {
+unsafeSetSubtract :: Take Set -> Take Set -> Give Set
+unsafeSetSubtract (Once set1) (Once set2) = [C.pure| isl_set* {
   isl_set_subtract($(isl_set* set1), $(isl_set* set2))
   } |]
 
-setSubtract :: Ptr Set -> Ptr Set -> Ptr Set
-setSubtract set1 set2 = unsafeSetSubtract (setCopy set1) (setCopy set2)
+setSubtract :: Keep Set -> Keep Set -> Give Set
+setSubtract set1 set2 = unsafeSetSubtract (copy' set1) (copy' set2)
 
 -- | Create an empty set
-setEmpty :: Ptr Space -> Ptr Set
-setEmpty space = [C.pure| isl_set* { isl_set_empty($(isl_space* space)) } |]
+setEmpty :: Take Space -> Give Set
+setEmpty (Once space) = [C.pure| isl_set* { isl_set_empty($(isl_space* space)) } |]
 
 -- | Create a universe set
-unsafeSetUniverse :: Ptr Space -> Ptr Set
-unsafeSetUniverse space = [C.pure| isl_set* {
+unsafeSetUniverse :: Take Space -> Give Set
+unsafeSetUniverse (Once space) = [C.pure| isl_set* {
   isl_set_universe($(isl_space* space))
   } |]
 
-setUniverse :: Ptr Space -> Ptr Set
-setUniverse = unsafeSetUniverse . spaceCopy
+setUniverse :: Keep Space -> Give Set
+setUniverse = unsafeSetUniverse . copy'
 
 -- | It is often useful to create objects that live in the same space as some
 -- other object. This can be accomplished by creating the new objects based on
 -- the space of the original object.
-setGetSpace :: Ptr Set -> Ptr Space
+setGetSpace :: Keep Set -> Give Space
 setGetSpace set = [C.pure| isl_space* {
   isl_set_get_space($(isl_set* set))
   } |]
 
 -- | The number of basic sets in a set can be obtained
-setNBasicSet :: Ptr Set -> CInt
+setNBasicSet :: Keep Set -> CInt
 setNBasicSet set = [C.pure| int { isl_set_n_basic_set($(isl_set* set)) } |]
 
-unsafeSetCoalesce :: Ptr Set -> Ptr Set
-unsafeSetCoalesce set =
+unsafeSetCoalesce :: Take Set -> Give Set
+unsafeSetCoalesce (Once set) =
   [C.pure| isl_set* { isl_set_coalesce($(isl_set* set)) } |]
 
 -- | Simplify the representation of a set by trying to combine pairs of basic
 -- sets into a single basic set.
-setCoalesce :: Ptr Set -> Ptr Set
-setCoalesce = unsafeSetCoalesce . setCopy
+setCoalesce :: Keep Set -> Give Set
+setCoalesce = unsafeSetCoalesce . copy'
 
 -- | Projection
-unsafeSetParams :: Ptr Set -> Ptr Set
-unsafeSetParams set = [C.pure| isl_set* { isl_set_params($(isl_set* set)) } |]
+unsafeSetParams :: Take Set -> Give Set
+unsafeSetParams (Once set)
+  = [C.pure| isl_set* { isl_set_params($(isl_set* set)) } |]
 
-setParams :: Ptr Set -> Ptr Set
-setParams = unsafeSetParams . setCopy
+setParams :: Keep Set -> Give Set
+setParams = unsafeSetParams . copy'
 
-unsafeSetComplement :: Ptr Set -> Ptr Set
-unsafeSetComplement set =
+unsafeSetComplement :: Take Set -> Give Set
+unsafeSetComplement (Once set) =
   [C.pure| isl_set* { isl_set_complement($(isl_set* set)) } |]
 
 -- | Projection
-setComplement :: Ptr Set -> Ptr Set
-setComplement = unsafeSetComplement . setCopy
+setComplement :: Keep Set -> Give Set
+setComplement = unsafeSetComplement . copy'
 
-setGetDimId :: Ptr Set -> DimType -> CUInt -> Ptr Id
+setGetDimId :: Keep Set -> DimType -> CUInt -> Give Id
 setGetDimId set ty pos =
   let ty' :: CInt
       ty' = fromDimType ty
@@ -205,8 +226,8 @@ setGetDimId set ty pos =
      )
      } |]
 
-unsafeSetProjectOut :: Ptr Set -> DimType -> CUInt -> CUInt -> Ptr Set
-unsafeSetProjectOut set ty first n =
+unsafeSetProjectOut :: Take Set -> DimType -> CUInt -> CUInt -> Give Set
+unsafeSetProjectOut (Once set) ty first n =
   let ty' :: CInt
       ty' = fromDimType ty
   in [C.pure| isl_set* {
@@ -218,20 +239,20 @@ unsafeSetProjectOut set ty first n =
      )
      } |]
 
-setProjectOut :: Ptr Set -> DimType -> CUInt -> CUInt -> Ptr Set
-setProjectOut set ty first n = unsafeSetProjectOut (setCopy set) ty first n
+setProjectOut :: Keep Set -> DimType -> CUInt -> CUInt -> Give Set
+setProjectOut set ty first n = unsafeSetProjectOut (copy' set) ty first n
 
 -- * BasicMap
 
 instance IslCopy BasicMap where copy = basicMapCopy
 instance IslFree BasicMap where free = basicMapFree
 
-basicMapCopy :: Ptr BasicMap -> Ptr BasicMap
+basicMapCopy :: Keep BasicMap -> Give BasicMap
 basicMapCopy bmap =
   [C.pure| isl_basic_map* { isl_basic_map_copy($(isl_basic_map* bmap)) } |]
 
-basicMapFree :: Ptr BasicMap -> IO ()
-basicMapFree bmap = void
+basicMapFree :: Take BasicMap -> IO ()
+basicMapFree (Once bmap) = void
   [C.block| isl_basic_map* { isl_basic_map_free($(isl_basic_map* bmap)); } |]
 
 -- * Map
@@ -239,23 +260,23 @@ basicMapFree bmap = void
 instance IslCopy Map where copy = mapCopy
 instance IslFree Map where free = mapFree
 
-mapCopy :: Ptr Map -> Ptr Map
+mapCopy :: Keep Map -> Give Map
 mapCopy map = [C.pure| isl_map* { isl_map_copy($(isl_map* map)) } |]
 
-mapFree :: Ptr Map -> IO ()
-mapFree map = void [C.block| isl_map* { isl_map_free($(isl_map* map)); } |]
+mapFree :: Take Map -> IO ()
+mapFree (Once map) = void [C.block| isl_map* { isl_map_free($(isl_map* map)); } |]
 
 -- * LocalSpace
 
 instance IslCopy LocalSpace where copy = localSpaceCopy
 instance IslFree LocalSpace where free = localSpaceFree
 
-localSpaceCopy :: Ptr LocalSpace -> Ptr LocalSpace
+localSpaceCopy :: Keep LocalSpace -> Give LocalSpace
 localSpaceCopy ls =
   [C.pure| isl_local_space* { isl_local_space_copy($(isl_local_space* ls)) } |]
 
-localSpaceFree :: Ptr LocalSpace -> IO ()
-localSpaceFree ls = void
+localSpaceFree :: Take LocalSpace -> IO ()
+localSpaceFree (Once ls) = void
   [C.block| isl_local_space* { isl_local_space_free($(isl_local_space* ls)); } |]
 
 -- * Space
@@ -263,45 +284,45 @@ localSpaceFree ls = void
 instance IslCopy Space where copy = spaceCopy
 instance IslFree Space where free = spaceFree
 
-spaceCopy :: Ptr Space -> Ptr Space
+spaceCopy :: Keep Space -> Give Space
 spaceCopy space =
   [C.pure| isl_space* { isl_space_copy($(isl_space* space)) } |]
 
-spaceFree :: Ptr Space -> IO ()
-spaceFree space = void
+spaceFree :: Take Space -> IO ()
+spaceFree (Once space) = void
   [C.block| isl_space* { isl_space_free($(isl_space* space)); } |]
 
-spaceIsEqual :: Ptr Space -> Ptr Space -> Bool
+spaceIsEqual :: Keep Space -> Keep Space -> Bool
 spaceIsEqual space1 space2 =
   [C.pure| isl_bool {
     isl_space_is_equal($(isl_space* space1), $(isl_space* space2)); }
   |]
 
-spaceHasEqualParams :: Ptr Space -> Ptr Space -> Bool
+spaceHasEqualParams :: Keep Space -> Keep Space -> Bool
 spaceHasEqualParams space1 space2 =
   [C.pure| isl_bool {
     isl_space_has_equal_params($(isl_space* space1), $(isl_space* space2)); }
   |]
 
-spaceHasEqualTuples :: Ptr Space -> Ptr Space -> Bool
+spaceHasEqualTuples :: Keep Space -> Keep Space -> Bool
 spaceHasEqualTuples space1 space2 =
   [C.pure| isl_bool {
     isl_space_has_equal_tuples($(isl_space* space1), $(isl_space* space2)); }
   |]
 
-spaceIsDomain :: Ptr Space -> Ptr Space -> Bool
+spaceIsDomain :: Keep Space -> Keep Space -> Bool
 spaceIsDomain space1 space2 =
   [C.pure| isl_bool {
     isl_space_is_domain($(isl_space* space1), $(isl_space* space2)); }
   |]
 
-spaceIsRange :: Ptr Space -> Ptr Space -> Bool
+spaceIsRange :: Keep Space -> Keep Space -> Bool
 spaceIsRange space1 space2 =
   [C.pure| isl_bool {
     isl_space_is_range($(isl_space* space1), $(isl_space* space2)); }
   |]
 
-spaceTupleIsEqual :: Ptr Space -> DimType -> Ptr Space -> DimType -> Bool
+spaceTupleIsEqual :: Keep Space -> DimType -> Keep Space -> DimType -> Bool
 spaceTupleIsEqual space1 type1 space2 type2 =
   let type1' :: CInt
       type1' = fromDimType type1
@@ -321,12 +342,12 @@ spaceTupleIsEqual space1 type1 space2 type2 =
 instance IslCopy Constraint where copy = constraintCopy
 instance IslFree Constraint where free = constraintFree
 
-constraintCopy :: Ptr Constraint -> Ptr Constraint
+constraintCopy :: Keep Constraint -> Give Constraint
 constraintCopy c =
   [C.pure| isl_constraint* { isl_constraint_copy($(isl_constraint* c)) } |]
 
-constraintFree :: Ptr Constraint -> IO ()
-constraintFree c = void
+constraintFree :: Take Constraint -> IO ()
+constraintFree (Once c) = void
   [C.block| isl_constraint* { isl_constraint_free($(isl_constraint* c)); } |]
 
 -- * Id
@@ -334,26 +355,28 @@ constraintFree c = void
 instance IslCopy Id where copy = idCopy
 instance IslFree Id where free = idFree
 
-idCopy :: Ptr Id -> Ptr Id
+idCopy :: Keep Id -> Give Id
 idCopy i = [C.pure| isl_id* { isl_id_copy($(isl_id* i)) } |]
 
-idFree :: Ptr Id -> IO ()
-idFree i = void [C.block| isl_id* { isl_id_free($(isl_id* i)); } |]
+idFree :: Take Id -> IO ()
+idFree (Once i) = void [C.block| isl_id* { isl_id_free($(isl_id* i)); } |]
 
 -- binary operations
 
-unsafeLocalSpaceIntersect :: Ptr LocalSpace -> Ptr LocalSpace -> Ptr LocalSpace
-unsafeLocalSpaceIntersect ls1 ls2 = [C.pure| isl_local_space* {
+unsafeLocalSpaceIntersect
+  :: Take LocalSpace -> Take LocalSpace -> Give LocalSpace
+unsafeLocalSpaceIntersect (Once ls1) (Once ls2) = [C.pure| isl_local_space* {
   isl_local_space_intersect(
     $(isl_local_space* ls1),
     $(isl_local_space* ls2)
   )
   } |]
 
-localSpaceIntersect :: Ptr LocalSpace -> Ptr LocalSpace -> Ptr LocalSpace
-localSpaceIntersect ls1 ls2 = unsafeLocalSpaceIntersect ls1 ls2
+localSpaceIntersect :: Keep LocalSpace -> Keep LocalSpace -> Give LocalSpace
+localSpaceIntersect ls1 ls2 = unsafeLocalSpaceIntersect (copy' ls1) (copy' ls2)
 
-unsafeBasicSetIntersectParams :: Ptr BasicSet -> Ptr BasicSet -> Ptr BasicSet
+unsafeBasicSetIntersectParams
+  :: Keep BasicSet -> Keep BasicSet -> Give BasicSet
 unsafeBasicSetIntersectParams bset1 bset2 = [C.pure| isl_basic_set* {
   isl_basic_set_intersect_params(
     $(isl_basic_set* bset1),
@@ -361,11 +384,11 @@ unsafeBasicSetIntersectParams bset1 bset2 = [C.pure| isl_basic_set* {
   )
   } |]
 
-basicSetIntersectParams :: Ptr BasicSet -> Ptr BasicSet -> Ptr BasicSet
+basicSetIntersectParams :: Keep BasicSet -> Keep BasicSet -> Give BasicSet
 basicSetIntersectParams bset1 bset2
   = unsafeBasicSetIntersectParams (basicSetCopy bset1) (basicSetCopy bset2)
 
-unsafeBasicSetIntersect :: Ptr BasicSet -> Ptr BasicSet -> Ptr BasicSet
+unsafeBasicSetIntersect :: Keep BasicSet -> Keep BasicSet -> Give BasicSet
 unsafeBasicSetIntersect bset1 bset2 = [C.pure| isl_basic_set* {
   isl_basic_set_intersect(
     $(isl_basic_set* bset1),
@@ -373,12 +396,12 @@ unsafeBasicSetIntersect bset1 bset2 = [C.pure| isl_basic_set* {
   )
   } |]
 
-basicSetIntersect :: Ptr BasicSet -> Ptr BasicSet -> Ptr BasicSet
+basicSetIntersect :: Keep BasicSet -> Keep BasicSet -> Give BasicSet
 basicSetIntersect bset1 bset2
   = unsafeBasicSetIntersect (basicSetCopy bset1) (basicSetCopy bset2)
 
-unsafeBasicSetListIntersect :: Ptr BasicSetList -> Ptr BasicSet
-unsafeBasicSetListIntersect list =
+unsafeBasicSetListIntersect :: Take BasicSetList -> Give BasicSet
+unsafeBasicSetListIntersect (Once list) =
   [C.pure| isl_basic_set* {
     isl_basic_set_intersect($(isl_basic_set_list* list))
   } |]
@@ -387,23 +410,22 @@ unsafeBasicSetListIntersect list =
 -- basicSetListIntersect :: Ptr BasicSetList -> Ptr BasicSet
 -- basicSetListIntersect = undefined
 
-unsafeSetIntersectParams :: Ptr Set -> Ptr Set -> Ptr Set
-unsafeSetIntersectParams set params = [C.pure| isl_set* {
+unsafeSetIntersectParams :: Take Set -> Take Set -> Give Set
+unsafeSetIntersectParams (Once set) (Once params) = [C.pure| isl_set* {
   isl_set_intersect_params( $(isl_set* set), $(isl_set* params))
   } |]
 
-setIntersectParams :: Ptr Set -> Ptr Set -> Ptr Set
+setIntersectParams :: Keep Set -> Keep Set -> Keep Set
 setIntersectParams set params
-  = unsafeSetIntersectParams (setCopy set) (setCopy params)
+  = unsafeSetIntersectParams (copy' set) (copy' params)
 
-unsafeSetIntersect :: Ptr Set -> Ptr Set -> Ptr Set
-unsafeSetIntersect set1 set2 = [C.pure| isl_set* {
+unsafeSetIntersect :: Take Set -> Take Set -> Give Set
+unsafeSetIntersect (Once set1) (Once set2) = [C.pure| isl_set* {
   isl_set_intersect($(isl_set* set1), $(isl_set* set2))
   } |]
 
-setIntersect :: Ptr Set -> Ptr Set -> Ptr Set
-setIntersect set1 set2 = unsafeSetIntersect (setCopy set1) (setCopy set2)
-
+setIntersect :: Keep Set -> Keep Set -> Give Set
+setIntersect set1 set2 = unsafeSetIntersect (copy' set1) (copy' set2)
 
 
 -- pwAffLeSet :: Ptr
